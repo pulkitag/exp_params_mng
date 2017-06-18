93dc9634a46b0747fd6532ebfb1f4b5f130bc5d3
pulkitag <pulkitag@berkeley.edu>
2017-06-18 00:02:40 -0700

    updated with tests etc.

--- params_object.py
+++ params_object.py
@@ -9,14 +9,19 @@ from os import path as osp
 import bson
 ##3rd party
 from pyhelper_fns import path_utils
-EXP_CLIENT = pymongo.MongoClient('localhost:27900')
+try:
+  import db_config
+  EXP_CLIENT = db_config.EXP_CLIENT
+except:
+  EXP_CLIENT = pymongo.MongoClient('localhost:27900')
 
 class ParamsObject(object):
   __metaclass__ = abc.ABCMeta
   _RESERVED_PARAM_NAMES_ = ['name', 'usertag']
-  def __init__(self, usertag=None, **params):
+  def __init__(self, usertag=None, userConfirm=True, **params):
     """
       usertag: a user specified tag to name the object
+      userConfirm: whether a user needs to confirm some consistency check operations 
       params : a set of parameters defining the abstract object
     """
     self._params = self.default_params()
@@ -33,82 +38,8 @@ class ParamsObject(object):
     self._expDb  = EXP_CLIENT[self.projectName]
     self._dbColl = self._expDb[self.name]
     #Check consistency
+    self.userConfirm = userConfirm
     self.check_params_consistency() 
-
-  @property
-  def params(self):
-    """
-    If new parameters are added, they are set to 
-    the default value
-    """
-    params = copy.deepcopy(self._params)
-    params['name'] = self.name
-    return params 
-
-  def params_no_reserved(self):
-    """Returns the params after stripping the reserved names"""
-    params = self.params
-    for k in self._RESERVED_PARAM_NAMES_:
-      del params[k] 
-    return params
-
-  def check_params_consistency(self):
-    """
-    mantain consistency of field names beween DB and default params
-    it is possible a user can add a new param name
-    we mantain the fact all entries have the same set
-    of parameters for all entries
-    """
-    print ('Checking consistency')
-    defParams = self.params
-    cursor    = self.dbColl.find()
-    if cursor.count() == 0:
-      return
-    doc = cursor.next()
-    #keys in the database
-    dbKeys = set(doc.keys())
-    #keys in the parameters
-    prKeys = set(defParams.keys())
-    prKeys.add('_id')
-    isOld  =  dbKeys.issubset(prKeys) and prKeys.issubset(dbKeys) 
-    if isOld:
-      return
-    if dbKeys.issubset(prKeys):
-      print ('#### New default parameters found ... ####')
-      #If new parameters are added
-      newKeys = prKeys.difference(dbKeys)
-      for k in newKeys:
-        print ('Setting all entries in {0} DB with value {1} for key {2}'.\
-               format(self.name, self.params[k], k))
-        ip = raw_input('Proceed (Y/N):')
-        assert ip == 'Y' or ip == 'N', 'Invalid Input, Enter Y/N'
-        if ip == 'Y':
-          docs = self.dbColl.find()
-          for d in docs:
-            newd = copy.deepcopy(d)
-            newd.update({k: self.params[k]})
-            self.dbColl.update(d, newd) 
-          print ('Updated')
-        else:
-          print ('Not proceeding with update')
-    else:
-      print ('#### Some default parameters are deleted ... ####')
-      newKeys = dbKeys.difference(prKeys)
-      for k in newKeys:
-        print ('For all entries in {0} DB, deleting key {1}'.\
-               format(self.name, k))
-        ip = raw_input('Proceed (Y/N):')
-        assert ip == 'Y' or ip == 'N', 'Invalid Input, Enter Y/N'
-        if ip == 'Y':
-          docs = self.dbColl.find()
-          for d in docs:
-            newd = copy.deepcopy(d)
-            del newd[k]
-            self.dbColl.update(d, newd) 
-          print ('Updated')
-        else:
-          print ('Not proceeding with update')
-
 
   @property
   def dbColl(self):
@@ -145,6 +76,122 @@ class ParamsObject(object):
     Returns:
       a dict containing default params
     """
+
+  @property
+  def params(self):
+    """
+    If new parameters are added, they are set to 
+    the default value
+    """
+    params = copy.deepcopy(self._params)
+    params['name'] = self.name
+    return params 
+
+  @property
+  def hashableParams(self):
+    """
+    Remove keys in params that have been specified in 
+    """
+    params = copy.deepcopy(self.params)
+    for k in self.ignoreHashKeys:
+      if k in params:
+        del params[k]
+    return params
+
+  @property
+  def defaultHashableParams(self):
+    """
+    default parameter values after the removing the
+    keys specifed in ignoreHashKeys
+    """
+    params = copy.deepcopy(self.default_params())
+    params['name'] = self.name
+    params['usertag'] = self._params['usertag']
+    for k in self.ignoreHashKeys:
+      if k in params:
+        del params[k]
+    return params
+
+  def params_no_reserved(self):
+    """Returns the params after stripping the reserved names"""
+    params = self.params
+    for k in self._RESERVED_PARAM_NAMES_:
+      del params[k] 
+    return params
+
+  def check_params_consistency(self):
+    """
+    mantain consistency of field names beween DB and default params
+    it is possible a user can add a new param name
+    we mantain the fact all entries have the same set
+    of parameters for all entries
+    """
+    print ('Checking consistency')
+    defParams = self.defaultHashableParams
+    cursor    = self.dbColl.find()
+    if cursor.count() == 0:
+      return
+    doc = cursor.next()
+    #keys in the database
+    dbKeys = set([str(k) for k in doc.keys()])
+    #keys in the parameters
+    prKeys = set(defParams.keys())
+    prKeys.add('_id')
+    isOld  =  dbKeys.issubset(prKeys) and prKeys.issubset(dbKeys) 
+    if isOld:
+      return
+    if dbKeys.issubset(prKeys):
+      print ('#### New default parameters found ... ####')
+      #If new parameters are added
+      newKeys = prKeys.difference(dbKeys)
+      for k in newKeys:
+        print ('Setting all entries in {0} DB with value {1} for key {2}'.\
+               format(self.name, defParams[k], k))
+        if self.userConfirm:
+          try:
+            #python2
+            ip = raw_input('Proceed (Y/N):')
+          except:
+            #python3
+            ip = input('Proceed (Y/N):')
+        else: 
+          ip = 'Y'
+        assert ip == 'Y' or ip == 'N', 'Invalid Input, Enter Y/N'
+        if ip == 'Y':
+          docs = self.dbColl.find()
+          for d in docs:
+            newd = copy.deepcopy(d)
+            newd.update({k: defParams[k]})
+            self.dbColl.update(d, newd) 
+          print ('Updated')
+        else:
+          print ('Not proceeding with update')
+    else:
+      print ('#### Some default parameters are deleted ... ####')
+      newKeys = dbKeys.difference(prKeys)
+      for k in newKeys:
+        print ('For all entries in {0} DB, deleting key {1}'.\
+               format(self.name, k))
+        if self.userConfirm:
+          try:
+            #python2
+            ip = raw_input('Proceed (Y/N):')
+          except:
+            #python3
+            ip = input('Proceed (Y/N):')
+        else: 
+          ip = 'Y'
+        assert ip == 'Y' or ip == 'N', 'Invalid Input, Enter Y/N'
+        if ip == 'Y':
+          docs = self.dbColl.find()
+          for d in docs:
+            newd = copy.deepcopy(d)
+            del newd[k]
+            self.dbColl.update(d, newd) 
+          print ('Updated')
+        else:
+          print ('Not proceeding with update')
+
  
   def find_by_id(self, _id):
     """
@@ -163,17 +210,24 @@ class ParamsObject(object):
     assert cur.count() == 1
     return cur.next()
 
-  def delete_by_id(self, _id):
+  def delete_by_id(self, _id, userConfirm=True):
     cur = self.find_by_id(_id)
     for dat in cur:
-      print ('Deleting\n {0}'.format(dat))
-      ip = raw_input('Proceed .. (Y/N)')
-      assert ip in ['Y', 'N']
-      if ip == 'Y':
-        self.dbColl.delete_one(dat)
-        print ('Deleted')
+      if userConfirm:
+        print ('Deleting\n {0}'.format(dat))
+        try:
+          #python2
+          ip = raw_input('Proceed .. (Y/N)')
+        except:
+          ip = input('Proceed .. (Y/N)')
+        assert ip in ['Y', 'N']
+        if ip == 'Y':
+          self.dbColl.delete_one(dat)
+          print ('Deleted')
+        else:
+          print ('Not Deleted') 
       else:
-        print ('Not Deleted') 
+        self.dbColl.delete_one(dat)
 
   def get_all_ids(self):
     """
@@ -186,12 +240,10 @@ class ParamsObject(object):
     return ids
  
   def hash_name(self):
-    cursor = self.dbColl.find(self.params)
+    cursor = self.dbColl.find(self.hashableParams)
     if cursor.count() == 0:
-      self.dbColl.insert_one(self.params)
-      cursor = self.dbColl.find(self.params)
+      self.dbColl.insert_one(self.hashableParams)
+      cursor = self.dbColl.find(self.hashableParams)
     assert cursor.count() == 1, 'Duplicates found'
     hashName = str(cursor.next()['_id'])
     return hashName
-
-
---
+++ tests/test_params_object.py
@@ -0,0 +1,119 @@
+import unittest
+from exp_params_mng import params_object
+
+
+class DummyObject(params_object.ParamsObject):
+  @property
+  def ignoreHashKeys(self):
+    return ['d']
+  
+  @property
+  def projectName(self):
+    return 'haptics'
+
+  def default_params(self):
+    dParams = {}
+    dParams['a'] = 0
+    dParams['b'] = 'zz'
+    dParams['c'] = 2
+    dParams['d'] = 3.
+    return dParams
+    
+## To test when a new default parameter is added
+class DummyObject2(params_object.ParamsObject):
+  @property
+  def name(self):
+    return 'DummyObject'
+
+  @property
+  def projectName(self):
+    return 'haptics' 
+
+  @property
+  def ignoreHashKeys(self):
+    return ['d']
+  
+  def default_params(self):
+    dParams = {}
+    dParams['a'] = 0
+    dParams['b'] = 'zz'
+    dParams['c'] = 2
+    dParams['d'] = 3.
+    dParams['e'] = 4
+    return dParams
+
+
+class ExperimentObjectTests(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    obj = DummyObject(userConfirm=False)
+    for i in obj.get_all_ids():
+      obj.delete_by_id(i, userConfirm=False)
+        
+  def tearDown(cls):
+    """
+    Delete all entries made during testing
+    """
+    obj = DummyObject(userConfirm=False)
+    for i in obj.get_all_ids():
+      obj.delete_by_id(i, userConfirm=False)      
+    
+  def test_ignore_hash(self):
+    #The key 'd' should be ignored
+    obj1 = DummyObject(a=0, d='5')
+    hashName1 = obj1.hash_name()
+    obj2 = DummyObject(a=0, d='6')
+    hashName2 = obj2.hash_name()
+    self.assertEqual(hashName1, hashName2)
+    self.assertEqual(len(obj2.get_all_ids()), 1)
+    self.assertEqual(obj1.params['d'], '5')
+    self.assertEqual(obj2.params['d'], '6')
+    obj3 = DummyObject(a=1, d='6')
+    hashName3 = obj3.hash_name()
+    self.assertEqual(len(obj2.get_all_ids()), 2)
+    self.assertNotEqual(hashName2, hashName3)
+    
+  def test_add_default_param(self):
+    obj1 = DummyObject(a=2, userConfirm=False)
+    hashName1 = obj1.hash_name()
+    #Default value is used
+    objDef      = DummyObject(userConfirm=False)
+    hashNameDef = objDef.hash_name()
+    #Create something with new defaults
+    obj2 = DummyObject2(userConfirm=False)
+    hashName2 = obj2.hash_name()
+    #import IPython; IPython.embed()
+    self.assertEqual(len(obj2.get_all_ids()), 2)
+    self.assertEqual(obj2.params['e'], 4)
+    for i in obj1.get_all_ids():
+      obj1.delete_by_id(i, userConfirm=False)
+    
+
+  def test_add_default_param_value(self):
+    """
+    Tests that added default parameter gets
+    added with the default value and not the user
+    specified value
+    """
+    obj1 = DummyObject(a=2, userConfirm=False)
+    hashName1 = obj1.hash_name()
+    #Default value is used
+    objDef      = DummyObject(userConfirm=False)
+    hashNameDef = objDef.hash_name()
+    #Create something with new defaults
+    obj2 = DummyObject2(userConfirm=False, e=5)
+    hashName2 = obj2.hash_name()
+    #import IPython; IPython.embed()
+    self.assertEqual(len(obj2.get_all_ids()), 3)
+    #Get obj1 again
+    obj  = DummyObject2(userConfirm=False)
+    objParams1 = obj.from_id(hashName1)
+    objParamsDef = obj.from_id(hashNameDef)
+    self.assertEqual(objParams1['e'], 4)
+    self.assertEqual(objParamsDef['e'], 4)
+    self.assertEqual(obj2.params['e'], 5)
+    for i in obj1.get_all_ids():
+      obj1.delete_by_id(i, userConfirm=False)  
+  
+if __name__ == '__main__':
+  unittest.main()
